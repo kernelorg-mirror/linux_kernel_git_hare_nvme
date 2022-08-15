18a916789ba7d2f36baf486214d58f9e8feb7fa1
Hannes Reinecke <hare@suse.de>
2022-08-15 08:36:36 +0200

    nvme: split off nvme_alloc_subsystem()
    Split of nvme_alloc_subsystem() to separate out initialisation
    from registration of the subsystem.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -2971,23 +2971,25 @@ static bool nvme_validate_cntlid(struct nvme_subsystem *subsys,
 	return true;
 }
 
-static int nvme_init_subsystem(struct nvme_ctrl *ctrl, struct nvme_id_ctrl *id)
+static struct nvme_subsystem *nvme_alloc_subsystem(struct nvme_ctrl *ctrl,
+			struct nvme_id_ctrl *id)
 {
-	struct nvme_subsystem *subsys, *found;
+	struct nvme_subsystem *subsys;
 	int ret;
 
 	subsys = kzalloc(sizeof(*subsys), GFP_KERNEL);
 	if (!subsys)
-		return -ENOMEM;
+		return NULL;
 
 	ret = ida_alloc(&nvme_subsystem_ida, GFP_KERNEL);
 	if (ret < 0) {
 		kfree(subsys);
-		return ret;
+		return NULL;
 	}
 	subsys->instance = ret;
 	mutex_init(&subsys->lock);
 	kref_init(&subsys->ref);
+	ida_init(&subsys->ns_ida);
 	INIT_LIST_HEAD(&subsys->ctrls);
 	INIT_LIST_HEAD(&subsys->nsheads);
 	nvme_init_subnqn(subsys, ctrl, id);
@@ -3003,14 +3005,6 @@ static int nvme_init_subsystem(struct nvme_ctrl *ctrl, struct nvme_id_ctrl *id)
 	else
 		subsys->subtype = NVME_NQN_NVME;
 
-	if (nvme_discovery_ctrl(ctrl) && subsys->subtype != NVME_NQN_DISC) {
-		dev_err(ctrl->device,
-			"Subsystem %s is not a discovery controller",
-			subsys->subnqn);
-		ida_free(&nvme_subsystem_ida, subsys->instance);
-		kfree(subsys);
-		return -EINVAL;
-	}
 	subsys->awupf = le16_to_cpu(id->awupf);
 	nvme_mpath_default_iopolicy(subsys);
 
@@ -3019,6 +3013,27 @@ static int nvme_init_subsystem(struct nvme_ctrl *ctrl, struct nvme_id_ctrl *id)
 	subsys->dev.groups = nvme_subsys_attrs_groups;
 	dev_set_name(&subsys->dev, "nvme-subsys%d", subsys->instance);
 	device_initialize(&subsys->dev);
+
+	return subsys;
+}
+
+static int nvme_init_subsystem(struct nvme_ctrl *ctrl, struct nvme_id_ctrl *id)
+{
+	struct nvme_subsystem *subsys, *found;
+	int ret;
+
+	subsys = nvme_alloc_subsystem(ctrl, id);
+	if (!subsys)
+		return -ENOMEM;
+
+	if (nvme_discovery_ctrl(ctrl) && subsys->subtype != NVME_NQN_DISC) {
+		dev_err(ctrl->device,
+			"Subsystem %s is not a discovery controller",
+			subsys->subnqn);
+		ida_free(&nvme_subsystem_ida, subsys->instance);
+		kfree(subsys);
+		return -EINVAL;
+	}
 
 	mutex_lock(&nvme_subsystems_lock);
 	found = __nvme_find_get_subsystem(subsys->subnqn);
@@ -3038,7 +3053,6 @@ static int nvme_init_subsystem(struct nvme_ctrl *ctrl, struct nvme_id_ctrl *id)
 			put_device(&subsys->dev);
 			goto out_unlock;
 		}
-		ida_init(&subsys->ns_ida);
 		list_add_tail(&subsys->entry, &nvme_subsystems);
 	}
 
