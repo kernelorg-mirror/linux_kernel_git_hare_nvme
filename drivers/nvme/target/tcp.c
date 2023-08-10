4e61914cf13636dea5bfc1c879ddf351c2533608
Hannes Reinecke <hare@suse.de>
2023-08-10 10:51:12 +0200

    nvmet-tcp: avoid circular locking dependency on install_queue()
    nvmet_tcp_install_queue() is driven from the ->io_work workqueue
    function, but will call flush_workqueue() which might trigger
    ->release_work() which in itself calls flush_work on ->io_work.
    
    To avoid that check for pending queue in disconnecting status,
    and return 'controller busy' until all disconnects are completed.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- drivers/nvme/target/tcp.c
+++ drivers/nvme/target/tcp.c
@@ -1845,8 +1845,18 @@ static u16 nvmet_tcp_install_queue(struct nvmet_sq *sq)
 		container_of(sq, struct nvmet_tcp_queue, nvme_sq);
 
 	if (sq->qid == 0) {
-		/* Let inflight controller teardown complete */
-		flush_workqueue(nvmet_wq);
+		struct nvmet_tcp_queue *q;
+		int pending = 0;
+
+		mutex_lock(&nvmet_tcp_queue_mutex);
+		list_for_each_entry(q, &nvmet_tcp_queue_list, queue_list) {
+			if (q->state == NVMET_TCP_Q_DISCONNECTING)
+				pending++;
+		}
+		mutex_unlock(&nvmet_tcp_queue_mutex);
+		/* Retry for pending controller teardown */
+		if (pending)
+			return NVME_SC_CONNECT_CTRL_BUSY;
 	}
 
 	queue->nr_cmds = sq->size * 2;
