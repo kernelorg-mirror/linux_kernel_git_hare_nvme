799fbf90fc690356d65e18fcfd1a9117091bb1f2
Hannes Reinecke <hare@suse.de>
2023-08-10 11:08:28 +0200

    nvmet-tcp: use 'spin_lock_bh' for state_lock()
    nvmet_tcp_schedule_release_queue() is called from socket state
    change callbacks, which may be called from an softirq context.
    So use 'spin_lock_bh' to avoid a spin lock warning.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- drivers/nvme/target/tcp.c
+++ drivers/nvme/target/tcp.c
@@ -1285,12 +1285,12 @@ done:
 
 static void nvmet_tcp_schedule_release_queue(struct nvmet_tcp_queue *queue)
 {
-	spin_lock(&queue->state_lock);
+	spin_lock_bh(&queue->state_lock);
 	if (queue->state != NVMET_TCP_Q_DISCONNECTING) {
 		queue->state = NVMET_TCP_Q_DISCONNECTING;
 		queue_work(nvmet_wq, &queue->release_work);
 	}
-	spin_unlock(&queue->state_lock);
+	spin_unlock_bh(&queue->state_lock);
 }
 
 static inline void nvmet_tcp_arm_queue_deadline(struct nvmet_tcp_queue *queue)
